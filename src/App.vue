8971b3f53a2cd0cb0c9ddef251227b9ba9d28291
Gabriel <gabriel02ramos@gmail.com>
2022-12-27 11:46:59 -0300

    estilizando as pages

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <Navbar/>
+  <Navbar :logo="logo_src" :alt="app_name"/>
   <router-view/>
   <Footer/>
 </template>
@@ -10,15 +10,39 @@
 
 <script>
   import Navbar from "./components/Navbar.vue";
-  import Footer from './components/Footer.vue'
+  import Footer from './components/Footer.vue';
+
   export default{
     components: {
       Navbar,
       Footer,
+    },
+    data() {
+      return {
+        logo_src: "/img/logo.png",
+        app_name: 'Maker Burger'
+      }
     }
   }
 </script>
 
 <style>
+  *{
+    font-family: Helvetica;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
 
+  .main-container{
+    margin: 50px;
+    min-height: 250px;
+  }
+
+  h1{
+    text-align: center;
+    font-size: 42px;
+    margin-bottom: 30px;
+    color: #222;
+  }
 </style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,20 @@
 <template>
-    <p>Opa</p>
+    <footer id="footer">
+        <p>Maker Burger &copy; 2022</p>
+    </footer>
 </template>
 
-<style>
-
+<style scoped>
+    #footer {
+        height: 200px;
+        background-color: #222;
+        border-top: 4px solid #111;
+        color: #fcba03;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+    }
 </style>
 
 <script>
