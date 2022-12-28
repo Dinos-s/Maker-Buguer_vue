f49134454bd2ccde8d5e9fd011fd71723910cded
Gabriel <gabriel02ramos@gmail.com>
2022-12-28 10:43:03 -0300

    criando e estilizando formulário

---
+++ src/components/Form.vue
@@ -0,0 +1,114 @@
+<template>
+    <div>
+        <p>comp msg</p>
+        <div>
+            <form id="burger-form">
+                <div class="input-container">
+                    <label for="nome">Cliente:</label>
+                    <input type="text" id="nome" name="nome" v-model="nome" placeholder="Digite o seu Nome">
+                </div>
+                <div class="input-container">
+                    <label for="pao">Escolha o pão:</label>
+                    <select name="pao" id="pao" v-model="pao">
+                        <option value="">Selecione o seu pão:</option>
+                        <option value="integral">Integral</option>
+                    </select>
+                </div>
+                <div class="input-container">
+                    <label for="carne">Escolha a sua Carne:</label>
+                    <select id="carne" name="carne" v-model="carne">
+                        <option value="">Selecione o tipo de carne:</option>
+                        <option value="frango">Frango</option>
+                    </select>
+                </div>
+                <div id="opcionais-container" class="input-container">
+                    <label id="opcionais-title" for="opicionais">Selecione os opcionais:</label>
+                    <div class="checkbox-container">
+                        <input type="checkbox" name="opcional" v-model="opcionais" value="salame">
+                        <span>Salame</span>
+                    </div>
+                </div>
+                <div class="input-container">
+                    <input type="submit" class="submit-btn" value="Criar Hamburger!">
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    #burger-form{
+        max-width: 400px;
+        margin: 0 auto;
+    }
+
+    .input-container{
+        display: flex;
+        flex-direction: column;
+        margin-bottom: 20px;
+    }
+
+    label{
+        font-weight: bold;
+        margin-bottom: 15px;
+        color: #222;
+        padding: 5px 10px;
+        border-left: 4px solid #fcba03;
+    }
+
+    input, select{
+        padding: 5px 10px;
+        width: 300px;
+    }
+
+    #opcionais-container{
+        flex-direction: row;
+        flex-direction: wrap;
+    }
+
+    #opcionais-title{
+        width: 100%;
+    }
+
+    .checkbox-container{
+        display: flex;
+        align-items: flex-start;
+        width: 50%;
+        margin-bottom: 20px;
+    }
+
+    .checkbox-container span, .checkbox-container input{
+        width: auto;
+    }
+
+    .checkbox-container span{
+        margin-left: 6px;
+        font-weight: bold;
+    }
+
+    .submit-btn{
+        background-color: #222;
+        color: #fcba03;
+        font-weight: bold;
+        border: 2px solid #222;
+        padding: 10px;
+        font-size: 16px;
+        margin: 0 auto;
+        cursor: pointer;
+        transition: .5s;
+    }
+
+    .submit-btn:hover{
+        background-color: transparent;
+        color: #222;
+    }
+</style>
+
+<script>
+    export default{
+        name: "Form",
+        data(){
+
+        }
+    }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <Banner/>
   <div class="main-container">
     <h1>Monte o seu Hamburger:</h1>
+    <Form/>
   </div>
 </template>
 
@@ -11,10 +12,12 @@
 
 <script>
   import Banner from "../components/Banner.vue";
+  import Form from "../components/Form.vue";
   export default{
     name: 'Home',
     components: {
       Banner,
+      Form,
     }
   }
 </script>
