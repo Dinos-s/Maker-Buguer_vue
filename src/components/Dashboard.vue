b1a27493cbae1f45e84b56f384ef60f5f66dddfd
Gabriel <gabriel02ramos@gmail.com>
2022-12-29 11:42:10 -0300

    dashboard dinâmica

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -11,15 +11,14 @@
             </div>
         </div>
         <div id="burger-table-rows">
-            <div class="burger-table-row">
-                <div class="order-number">1</div>
-                <div>GMR</div>
-                <div>3 queijos</div>
-                <div>Frago</div>
+            <div class="burger-table-row" v-for="burger in burgers" :key="burger.id">
+                <div class="order-number">{{ burger.id }}</div>
+                <div>{{ burger.nome }}</div>
+                <div>{{ burger.pao }}</div>
+                <div>{{ burger.carne }}</div>
                 <div>
                     <ul>
-                        <li>Tomate</li>
-                        <li>Cebola Roxa</li>
+                        <li v-for="(opcional, index) in burger.opcionais" :key="index">{{ opcional }}</li>
                     </ul>
                 </div>
                 <div>
@@ -94,5 +93,26 @@
 <script>
     export default{
         name: "Dashboard",
+        data() {
+            return {
+                burgers: null,
+                burger_id: null,
+                status: []
+            }
+        },
+        methods: {
+            async getPedidos(){
+                const req = await fetch("http://localhost:3000/burgers")
+
+                const data = await req.json()
+
+                this.burgers = data;
+
+                console.log(this.burgers);
+            },
+        },
+        mounted() {
+            this.getPedidos();
+        }
     }
 </script>
