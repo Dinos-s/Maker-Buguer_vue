e4ee1228e697984bdc99052b16146c85a1efd4ee
Gabriel <gabriel02ramos@gmail.com>
2022-12-29 21:09:56 -0300

    Deletando Sanduba

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -26,7 +26,7 @@
                         <option value="">Status</option>
                         <option v-for="s in status" :key="s.id" value="s.tipo" :selected="burger.status == s.tipo">{{ s.tipo }}</option>
                     </select>
-                    <button class="delete-btn">Cancelar</button>
+                    <button class="delete-btn" @click="deleteBurger(burger.id)">Cancelar</button>
                 </div>
             </div>
         </div>
@@ -121,8 +121,19 @@
                 this.status = data;
 
                 console.log(this.status);
+            },
+            async deleteBurger(id){
+                const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+                    method: "DELETE"
+                });
+
+                const res = await req.json();
+
+                //msg
+
+                this.getPedidos()
             }
-        },
+        }, 
         mounted() {
             this.getPedidos();
         }
